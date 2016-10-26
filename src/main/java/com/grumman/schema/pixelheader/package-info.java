b8f161cf966bef4587fb9121a7c0aee27c483564
Alexander Arend <Alexander Arend>
2016-10-26 16:56:07 -0400

    Added headers and interceptor

---
+++ src/main/java/com/grumman/schema/pixelheader/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.grumman.com/schema/PixelHeader", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package com.grumman.schema.pixelheader;
